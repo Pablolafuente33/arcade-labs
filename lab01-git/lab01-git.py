e74dca067ae35fcb31ab4f57249a71f363101856
Pablolafuente33 <pablo.lafuente@edu.uah.es>
2026-02-12 16:01:36 +0100

    Commit 2, circulo creado

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -7,8 +7,7 @@ arcade.open_window(WIDTH, HEIGHT, "Example")
 
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
-arcade.draw_circle_filled(400, 300, 50, arcade.color.BLUE)
-arcade.draw_rectangle_filled(200, 150, 100, 50, arcade.color.RED)
+arcade.draw_circle_filled(400, 200, 50, arcade.color.BLUE)
 arcade.finish_render()
 
 arcade.run()
