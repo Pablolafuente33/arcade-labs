d87871075b0ef4da195e555e1fce87950316f257
Pablolafuente33 <pablo.lafuente@edu.uah.es>
2026-02-12 15:57:41 +0100

    Commit nuevo

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -7,6 +7,8 @@ arcade.open_window(WIDTH, HEIGHT, "Example")
 
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
+arcade.draw_circle_filled(400, 300, 50, arcade.color.BLUE)
+arcade.draw_rectangle_filled(200, 150, 100, 50, arcade.color.RED)
 arcade.finish_render()
 
 arcade.run()
