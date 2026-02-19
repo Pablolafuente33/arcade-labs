db9da26e5b7d8e30ff3cf779df22a0510b4d2a58
Pablolafuente33 <pablo.lafuente@edu.uah.es>
2026-02-19 15:16:45 +0100

    Solucionando sincronización final

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -6,7 +6,7 @@ HEIGHT = 600
 arcade.open_window(WIDTH, HEIGHT, "Example")
 
 arcade.start_render()
-arcade.draw_text("Hola mundo", 350, 300, arcade.color.WHITE)
+arcade.draw_text("Hello!!", 350, 300, arcade.color.WHITE)
 arcade.draw_circle_filled(400, 200, 50, arcade.color.BLUE)
 arcade.draw_rect_filled(arcade.XYWH(55, 55, 100, 100), arcade.color.BLACK_BEAN)
 arcade.finish_render()
